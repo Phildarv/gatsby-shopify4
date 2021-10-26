ece023655cacd467450b9230f83bc106330b5169
Phildarv <phillip@darvill.co.uk>
2021-10-26 22:36:17 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
         password: process.env.SHOPIFY_SHOP_PASSWORD,
         storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
         shopifyConnections: ["collections"],
-      },}
-       {
+      },
+       
       resolve: '@plasmicapp/loader-gatsby',
       options: {
         projects: [
